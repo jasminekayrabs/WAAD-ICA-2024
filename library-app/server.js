989d93ef8ef5883596828835bb77f3ae0c206916
jasminekayrabs <80991158+jasminekayrabs@users.noreply.github.com>
2024-04-22 01:07:51 +0200

    modify server.js

--- library-app/server.js
+++ library-app/server.js
@@ -1,28 +1,9 @@
+require('dotenv').config();
 const express = require('express');
-const dotenv = require('dotenv');
-const db = require('./db');
-
-dotenv.config();
 const app = express();
-app.use(express.json()); 
 
-app.get('/', (req, res) => {
-  res.send('Library App API');
+app.use(express.json()); // For parsing application/json
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });
-
-// Test route to query the database
-app.get('/test-db', async (req, res) => {
-    try {
-      const { rows } = await db.query('SELECT NOW()');
-      res.json(rows);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Server Error');
-    }
-  });
-
-const authRoutes = require('./routes/authRoutes'); 
-app.use('/api/auth', authRoutes);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
