019f9f7749313e2cf6d20734f58409e68bc5d25b
jasminekayrabs <80991158+jasminekayrabs@users.noreply.github.com>
2024-05-14 14:24:26 +0200

    Updating middleware for authorization

--- library-app/middleware/authorize.js
+++ library-app/middleware/authorize.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization;  // Directly use the token without splitting
+    const token = req.headers.authorization; 
 
     if (!token) {
         return res.status(401).json({ error: 'Access denied. No token provided.' });
